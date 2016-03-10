b590edc075a6830fe5465dd0ef49f22509a64a22
Vasil Vanchuk <v.vanchuk@tut.by>
2016-03-10 18:09:15 +0300

    Add printable

---
+++ 2.3/printable.hs
@@ -0,0 +1,25 @@
+{-
+Реализуйте класс типов Printable, предоставляющий один метод toString — функцию одной переменной, которая преобразует значение типа, являющегося представителем Printable, в строковое представление.
+
+Сделайте типы данных Bool и () представителями этого класса типов, обеспечив следующее поведение:
+
+GHCi> toString True
+"true"
+GHCi> toString False
+"false"
+GHCi> toString ()
+"unit type"
+-}
+
+class Printable a
+ where toString :: a -> String
+
+instance Printable Bool
+ where    toString True = "true"
+          toString False = "false"
+
+instance Printable ()
+ where toString _ = "unit type"
+
+instance (Printable a, Printable b) => Printable (a,b)
+ where toString pair = "(" ++ (toString $ fst pair) ++ "," ++ (toString $ snd pair) ++ ")"
